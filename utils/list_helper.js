f63c841d000a2504d0a5bf43f65be4f7d2757935
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-05-19 18:22:47 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,12 @@
-const dummy = (blogs) => {
-  return 1
+const dummy = () => 1
+
+const totalLikes = (blogs) => {
+  return blogs
+    .map(blog => blog.likes)
+    .reduce((total, amount) => total + amount, 0)
 }
 
 module.exports = {
   dummy,
+  totalLikes,
 }
