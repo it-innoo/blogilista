b85dee1dece1b5fa57f81cec07f62e150e943003
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-05-24 01:16:39 +0300

    4.10: blogilistan testit, step3

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 
-const { PORT } = process.env
+let { PORT } = process.env
 let { MONGODB_URI } = process.env
 
 if (process.env.NODE_ENV === 'test') {
+  PORT = process.env.TEST_PORT
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
+
 module.exports = {
   MONGODB_URI,
   PORT,
