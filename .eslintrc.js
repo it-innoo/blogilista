d0db437b7736f9821c1c226db94ed7f2e6a79706
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-05-23 14:58:49 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
     "env": {
         "node": true,
-        "commonjs": true,
         "es6": true,
         "jest": true
     },
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,12 +1,19 @@
 const dummy = () => 1
 
-const totalLikes = (blogs) => {
-  return blogs
-    .map(blog => blog.likes)
-    .reduce((total, amount) => total + amount, 0)
+const totalLikes = blogs => blogs
+  .map(blog => blog.likes)
+  .reduce((total, amount) => total + amount, 0)
+
+const favoriteBlog = (blogs) => {
+  const [{
+    _id, __v, url, ...favorites
+  }] = blogs || [{ _id: '0', __v: '', url: '' }]
+
+  return favorites
 }
 
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes,
 }
