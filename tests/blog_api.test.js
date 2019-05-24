4c98a029cd60ffbff20d71109d05297a1c379117
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-05-24 20:55:21 +0300

    4.13 blogilistan laajennus, step1

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,116 +8,144 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
+describe('tietokannassa on alunperin muutama blogi', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
 
-  const blogObjects = helper.initialBlogs
-    .map(blog => new Blog(blog))
-  const promiseArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promiseArray)
-})
+    const blogObjects = helper.initialBlogs
+      .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body.length).toBe(helper.initialBlogs.length)
-})
+    expect(response.body.length).toBe(helper.initialBlogs.length)
+  })
 
-test('identier is named id', async () => {
-  const response = await api.get('/api/blogs')
+  test('identier is named id', async () => {
+    const response = await api.get('/api/blogs')
 
-  const ids = response.body.map(r => r.id)
-  expect(ids).toBeDefined()
-})
+    const ids = response.body.map(r => r.id)
+    expect(ids).toBeDefined()
+  })
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Async/Await',
-    author: 'Me Luv',
-    url: 'url',
-    likes: 0,
-  }
+  describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'Async/Await',
+        author: 'Me Luv',
+        url: 'url',
+        likes: 0,
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  const title = blogsAtEnd.map(r => r.title)
+      const blogsAtEnd = await helper.blogsInDb()
+      const title = blogsAtEnd.map(r => r.title)
 
-  expect(blogsAtEnd.length)
-    .toBe(helper.initialBlogs.length + 1)
+      expect(blogsAtEnd.length)
+        .toBe(helper.initialBlogs.length + 1)
 
-  expect(title)
-    .toContain(
-      'Async/Await',
-    )
-})
+      expect(title)
+        .toContain(
+          'Async/Await',
+        )
+    })
 
-test('a valid blog added defaults to zero likes', async () => {
-  const newBlog = {
-    title: 'Async/Await',
-    author: 'Me Luv',
-    url: 'url',
-  }
+    test('likes defaults to zero', async () => {
+      const newBlog = {
+        title: 'Async/Await',
+        author: 'Me Luv',
+        url: 'url',
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  const likes = blogsAtEnd
-    .filter(r => r.title === 'Async/Await')
-    .map(r => r.likes)
+      const blogsAtEnd = await helper.blogsInDb()
+      const likes = blogsAtEnd
+        .filter(r => r.title === 'Async/Await')
+        .map(r => r.likes)
 
-  expect(blogsAtEnd.length)
-    .toBe(helper.initialBlogs.length + 1)
+      expect(blogsAtEnd.length)
+        .toBe(helper.initialBlogs.length + 1)
 
-  expect(likes[0])
-    .toBe(0)
-})
+      expect(likes[0])
+        .toBe(0)
+    })
 
-test('blog without title is not added', async () => {
-  const newBlog = {
-    author: 'Me Luv',
-    url: 'url',
-  }
+    test('fails with status code 400 if title invalid', async () => {
+      const newBlog = {
+        author: 'Me Luv',
+        url: 'url',
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd.length)
-    .toBe(helper.initialBlogs.length)
-})
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd.length)
+        .toBe(helper.initialBlogs.length)
+    })
 
-test('blog without url is not added', async () => {
-  const newBlog = {
-    title: 'Async/Await',
-    author: 'Me Luv',
-  }
+    test('fails with status code 400 if url invalid', async () => {
+      const newBlog = {
+        title: 'Async/Await',
+        author: 'Me Luv',
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd.length)
-    .toBe(helper.initialBlogs.length)
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd.length)
+        .toBe(helper.initialBlogs.length)
+    })
+  })
+
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd.length)
+        .toBe(helper.initialBlogs.length - 1)
+
+      const title = blogsAtEnd.map(r => r.title)
+      expect(title).not.toContain(blogToDelete.title)
+    })
+
+    test('fails with status code 400 if id is invalid', async () => {
+      await api
+        .delete('/api/blogs/1')
+        .expect(400)
+    })
+  })
 })
 
 afterAll(() => {
