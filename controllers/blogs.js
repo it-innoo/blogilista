6dc07937ccb70d99cdc145d821364e5bb9cacae9
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-05-24 14:32:41 +0300

    4.12*: blogilistan testit, step5 refactored

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,15 +6,21 @@ blogsRouter.get('/', async (_request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+  const { body } = request
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog.toJSON())
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = blogsRouter
