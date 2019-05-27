e0c68786118da44f27704ded85c5e8b6f636181f
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-05-28 00:23:35 +0300

    4.16*: blogilistan laajennus, step5

--- controllers/users.js
+++ controllers/users.js
@@ -7,9 +7,20 @@ usersRouter.get('/', async (request, response) => {
   response.json(users.map(u => u.toJSON()))
 })
 
+// eslint-disable-next-line consistent-return
 usersRouter.post('/', async (request, response, next) => {
   try {
     const { body } = request
+
+    if (body.password === undefined
+    || body.password === null
+    || body.password.length < 3) {
+      return response
+        .status(400)
+        .json(
+          { error: 'password is required and must be at least 3 chars' },
+        )
+    }
 
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
