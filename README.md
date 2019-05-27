01c6e11137d222e228b477766d4edc9227fcd5d5
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-05-27 14:20:30 +0300

    4.15: blogilistan laajennus, step4

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tehtäviä
+Rakennamme tämän osan tehtävissä blogilistasovellusta, jonka avulla käyttäjien on mahdollista tallettaa tietoja internetistä löytämistään mielenkiintoisista blogeista. Kustakin blogista talletetaan sen kirjoittaja (author), aihe (title), url sekä blogilistasovelluksen käyttäjien antamien äänien määrä.
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,6 +5,7 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const api = supertest(app)
 
@@ -165,6 +166,48 @@ describe('tietokannassa on alunperin muutama blogi', () => {
       const blog = blogsAtEnd[0]
       expect(blog.likes).toBe(likes + 1)
     })
+  })
+})
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const user = new User({ username: 'root', password: 'sekret' })
+    await user.save()
+  })
+
+  test('users are returned as json', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usernames = usersAtStart.map(u => u.username)
+    expect(usernames).toContain('root')
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
   })
 })
 
